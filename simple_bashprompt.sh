8318492a5f943e9431f0e37d93674de9b4f96973
david <davidjohansson35@gmail.com>
2019-07-07 22:14:15 +0200

    Automated update of promp with simple_prompt.sh

--- simple_bashprompt.sh
+++ simple_bashprompt.sh
@@ -1,4 +1,6 @@
 # Should create a simple bash prompt that just prints the current user
-# and the basename of current PWD.
+# and the basename of current PWD. Run with source to have it update
+# directly
 
 echo 'PS1="\[\033[01;32m\]\u@\W\033[00m:"' >> ~/.bashrc
+source ~/.bashrc
