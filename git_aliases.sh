606e332a33b4dc9beb8b9203557c49559b2a218f
david <davidjohansson35@gmail.com>
2019-07-07 21:04:50 +0200

    Git alias script added

---
+++ git_aliases.sh
@@ -0,0 +1,7 @@
+# Some basic aliases for git
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
