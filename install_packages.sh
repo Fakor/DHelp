10eae463692c4d9c248eb17a2a1ca43a52f6a3b7
david <davidjohansson35@gmail.com>
2019-07-08 22:18:08 +0200

    Install package script added

---
+++ install_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get update
+
+apt-get install git -y
+apt-get install emacs25 -y
+apt install python-pip -y
+apt install python3-pip -y
+apt-get install python3-venv -y
+apt-get install python3-tk -y
