40b334763dd1e74e0dc34a36e7b1d0ac8124723b
david <davidjohansson35@gmail.com>
2019-07-13 22:15:18 +0200

    Prettier hist alias

--- git_aliases.sh
+++ git_aliases.sh
@@ -4,4 +4,4 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.hist "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short"
