b1c93491beba5aa27fa48765f8f855ead7371018
david <davidjohansson35@gmail.com>
2019-09-07 16:49:32 +0200

    create_webuser now also adds firefox lib

--- create_webuser.sh
+++ create_webuser.sh
@@ -4,9 +4,13 @@
 NEW_USER=$1
 
 FIREFOX_BIN=/usr/bin/firefox
+FIREFOX_LIB=/usr/lib/firefox/firefox
 WEBUSERS=webusers
 
 addgroup $WEBUSERS
 chmod 750 $FIREFOX_BIN
+chmod 750 $FIREFOX_LIB
+
 chown root:$WEBUSERS $FIREFOX_BIN
+chown root:$WEBUSERS $FIREFOX_LIB
 adduser $NEW_USER $WEBUSERS
